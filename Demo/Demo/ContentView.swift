4378caac617a1e806842280801577d08743add27
George Cox <george@tundaware.com>
2020-01-24 10:24:07 -0500

    Adds Demo project

---
+++ Demo/Demo/ContentView.swift
@@ -0,0 +1,17 @@
+///
+///  Created by George Cox on 1/24/20.
+///
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Demo/Pods/Target Support Files/NanoID/NanoID-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NanoID : NSObject
+@end
+@implementation PodsDummy_NanoID
+@end
